0cfeccb928d8f14ef4090304c68c668c9e6953a3
johny english <pabx06@gmail.com>
2020-09-13 14:51:41 +0200

    second commit

---
+++ scripts/download.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wget https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/armeb-linux-gnueabi/gcc-linaro-7.5.0-2019.12-x86_64_armeb-linux-gnueabi.tar.xz
+wget https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/armeb-linux-gnueabi/gcc-linaro-7.5.0-2019.12-x86_64_armeb-linux-gnueabi.tar.xz.asc
+wget https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/armeb-linux-gnueabi/runtime-gcc-linaro-7.5.0-2019.12-armeb-linux-gnueabi.tar.xz
+wget https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/armeb-linux-gnueabi/runtime-gcc-linaro-7.5.0-2019.12-armeb-linux-gnueabi.tar.xz.asc
+wget https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/armeb-linux-gnueabi/sysroot-glibc-linaro-2.25-2019.12-armeb-linux-gnueabi.tar.xz
+wget https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/armeb-linux-gnueabi/sysroot-glibc-linaro-2.25-2019.12-armeb-linux-gnueabi.tar.xz.asc
+wget https://master.dl.sourceforge.net/project/libpng/libpng12/older-releases/1.2.29/libpng-1.2.29.tar.gz
+wget https://www.zlib.net/fossils/zlib-1.2.3.tar.gz
+wget https://github.com/manugarg/pacparser/releases/download/1.3.7/pacparser-1.3.7.tar.gz
+wget https://github.com/openssl/openssl/archive/OpenSSL_1_0_1j.tar.gz
+wget https://freefr.dl.sourceforge.net/project/gsoap2/gsoap-2.7/gsoap_2.7.13.zip
+wget https://archive.mozilla.org/pub/spidermonkey/releases/1.7.0/js-1.7.0.tar.gz
+wget https://github.com/libexpat/libexpat/archive/R_2_1_0.tar.gz
---
+++ scripts/extract.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+tar -xf ../downloads/gcc-linaro-7.5.0-2019.12-x86_64_armeb-linux-gnueabi.tar.xz
+tar -xf ../downloads/gcc-linaro-7.5.0-2019.12-x86_64_armeb-linux-gnueabi.tar.xz
+tar -xf ../downloads/libpng-1.2.29.tar.gz
+tar -xf ../downloads/zlib-1.2.3.tar.gz
+tar -xf ../downloads/pacparser-1.3.7.tar.gz
+tar -xf ../downloads/OpenSSL_1_0_1j.tar.gz
+tar -xf ../downloads/gsoap_2.7.13.zip
+tar -xf ../downloads/js-1.7.0.tar.gz
+tar -xf ../downloads/R_2_1_0.tar.gz
