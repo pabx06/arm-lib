b870c98f5c4f2f71df5d3ab5a6ced8cfc0ab0071
johny english <pabx06@gmail.com>
2020-09-13 19:14:33 +0200

    sourceforge slow fix

--- scripts/download.sh
+++ scripts/download.sh
@@ -11,6 +11,6 @@ wget https://master.dl.sourceforge.net/project/libpng/libpng12/older-releases/1.
 wget https://www.zlib.net/fossils/zlib-1.2.3.tar.gz
 wget https://github.com/manugarg/pacparser/releases/download/1.3.7/pacparser-1.3.7.tar.gz
 wget https://github.com/openssl/openssl/archive/OpenSSL_1_0_1j.tar.gz
-wget https://freefr.dl.sourceforge.net/project/gsoap2/gsoap-2.7/gsoap_2.7.13.zip
+#wget https://freefr.dl.sourceforge.net/project/gsoap2/gsoap-2.7/gsoap_2.7.13.zip
 wget https://archive.mozilla.org/pub/spidermonkey/releases/1.7.0/js-1.7.0.tar.gz
 wget https://github.com/libexpat/libexpat/archive/R_2_1_0.tar.gz
